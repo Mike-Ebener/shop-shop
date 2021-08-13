8e7edf1286ee9c7b7bdd3c41ee1c16a7f0c7f165
ME <mebener44@gmail.com>
2021-08-12 22:59:20 -0700

    cart built

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,10 +3,18 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+
+
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
+import Cart from '../components/Cart';
 
 
 function Detail() {
@@ -17,7 +25,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -30,9 +38,33 @@ function Detail() {
     }
   }, [products, data, dispatch, id]);
 
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  }
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+
+  };
+
   return (
     <>
-      {currentProduct ? (
+      {currentProduct && cart ? (
         <div className="container my-1">
           <Link to="/">
             ← Back to Products
@@ -48,10 +80,13 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button 
+              disabled={!cart.find(p => p._id === currentProduct._id)} 
+              onClick={removeFromCart}
+            >
               Remove from Cart
             </button>
           </p>
@@ -65,9 +100,101 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
 
 export default Detail;
+
+
+// function Detail() {
+//   const [state, dispatch] = useStoreContext();
+//   const { id } = useParams();
+
+//   const [currentProduct, setCurrentProduct] = useState({})
+
+//   const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+//   const { products, cart } = state;
+
+//   useEffect(() => {
+//     if (products.length) {
+//       setCurrentProduct(products.find(product => product._id === id));
+//     } else if (data) {
+//       dispatch({
+//         type: UPDATE_PRODUCTS,
+//         products: data.products
+//       });
+//     }
+//   }, [products, data, dispatch, id]);
+
+//   const addToCart = () => {
+//     const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+//     if (itemInCart) {
+//       dispatch({
+//         type: UPDATE_CART_QUANTITY,
+//         _id: id,
+//         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+//       });
+//     } else {
+//       dispatch({
+//         type: ADD_TO_CART,
+//         product: { ...currentProduct, purchaseQuantity: 1 }
+//       });
+//     }
+//   };
+
+//   const removeFromCart = () => {
+//     dispatch({
+//       type: REMOVE_FROM_CART,
+//       _id: currentProduct._id
+//     });
+//   };
+
+//   return (
+//     <>
+//       {currentProduct ? (
+//         <div className="container my-1">
+//           <Link to="/">
+//             ← Back to Products
+//           </Link>
+
+//           <h2>{currentProduct.name}</h2>
+
+//           <p>
+//             {currentProduct.description}
+//           </p>
+
+//           <p>
+//             <strong>Price:</strong>
+//             ${currentProduct.price}
+//             {" "}
+//             <button>
+//               Add to Cart
+//             </button>
+//             <button
+//               disabled={!cart.find(p => p._id === currentProduct._id)}
+//               onClick={removeFromCart}
+//             >
+//               Remove from Cart
+//             </button>
+//           </p>
+
+//           <img
+//             src={`/images/${currentProduct.image}`}
+//             alt={currentProduct.name}
+//           />
+//         </div>
+//       ) : null}
+//       {
+//         loading ? <img src={spinner} alt="loading" /> : null
+//       }
+//       <Cart />
+//     </>
+//   );
+// };
+
+// export default Detail;
 
